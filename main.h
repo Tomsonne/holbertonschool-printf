1b5b4219cc4159d6908f9705a32112e7e2384b94
thomas rousseau <thomas.rousseau19@gmail.com>
2025-03-26 11:46:17 +0100

    betty style main.h

--- main.h
+++ main.h
@@ -7,9 +7,8 @@ int print_char(va_list args);
 int print_string(va_list args);
 int print_int(va_list args);
 void recursion_print_int(unsigned int num);
-
 int print_validate(char format);
-int print_func_format(const char format,va_list args);
+int print_func_format(const char format, va_list args);
 int print_all(const char *format, va_list args);
 int _write(char c);
 int _printf(const char *format, ...);
