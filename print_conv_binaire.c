7a899a696b788812ccc557a36a1937425b60550a
thomas rousseau <thomas.rousseau19@gmail.com>
2025-03-28 21:26:39 +0100

    betty style print_conv_binaire

--- print_conv_binaire.c
+++ print_conv_binaire.c
@@ -5,14 +5,10 @@
 
 
 /**
- * print_char - Écris un caractere
+ * print_conv_binaire - Écris un caractere
  * @args: type
  * Return: 1 ( pas d'erreur )
  */
- 
-  // pour avoir du binaire il faut diviser par 2 autant de fois jusqu'à ce que ça soit plus divisible par 2.
- // le reste de la division (modulo) est lu dans le sens du bas vers le haut. elle sera forcement stocké dans un tableau.
- // mais je n'arrive pas à faire l'algo. Merci de votre aide
 
 
 int print_conv_binaire(va_list args)
@@ -21,29 +17,27 @@ int index = 0;
 int cpindex;
 char *binaire;
 int num = va_arg(args, int);
- 
-binaire = malloc(33); 
- 
-while(num > 0)
+
+binaire = malloc(33);
+while (num > 0)
 {
-    if (num%2 == 1)
-    {
-        binaire[index] = '1';
-        index++;
-    }
-    else
-    {
-        binaire[index] = '0';
-        index++;
-    }
-    num = num / 2;
-}
-cpindex = index-1;
-while(cpindex!=0)
+if (num % 2 == 1)
 {
-    _write(binaire[cpindex]);
-    cpindex--;
+binaire[index] = '1';
+index++;
 }
- 
-return (index-1);    
+else
+{
+binaire[index] = '0';
+index++;
+}
+num = num / 2;
+}
+cpindex = index - 1;
+while (cpindex != 0)
+{
+_write(binaire[cpindex]);
+cpindex--;
+}
+return (index - 1);
 }
